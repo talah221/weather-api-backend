9ce4aa36cc49732c73efdaf4e62d34cc4dc07d03
tallahyani <talni250@gmail.com>
2022-07-09 21:26:01 +0300

    port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:"http://localhost:3000"})
+  app.enableCors({origin:["http://localhost:3000","https://weather-app-tal.herokuapp.com/"]})
   await app.listen(process.env.PORT || '3030');
 }
 bootstrap();
