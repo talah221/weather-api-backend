50cb3f772488243641015dd817979d8e9d0d23f7
tallahyani <talni250@gmail.com>
2022-07-10 23:50:28 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,25 @@ import { GlobalModule } from './global.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GlobalModule);
-  app.enableCors({origin:["http://localhost:3000","https://weather-app-tal.herokuapp.com/"]})
+
+  const cors_origins: string[] =["http://localhost:3000","https://weather-app-tal.herokuapp.com"]
+
+
+  app.enableCors({
+    origin:
+      (origin, callback) => {
+        if (!origin || cors_origins.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS :( '));
+        }
+      },
+
+
+    credentials: true,
+  });
+
+
   await app.listen(process.env.PORT || '3030');
 }
 bootstrap();
